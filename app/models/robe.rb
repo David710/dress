cccb8ffbc909d34fe75494f42b975311ef8a4cec
David710 <bednarski_david@hotmail.com>
2018-03-07 22:08:02 +0100

    knn coded in ruby

--- app/models/robe.rb
+++ app/models/robe.rb
@@ -1,4 +1,4 @@
-require 'knn'
+# require 'knn'
 class Robe < ApplicationRecord
 
   def nearest_neighbours(data, nb_neighbours)
@@ -18,4 +18,37 @@ class Robe < ApplicationRecord
 
   end
 
+end
+
+class KNN
+  def initialize(data, options={})
+    @data = data
+    @distance_measure = options[:distance_measure] || :euclidean_distance
+  end
+
+  def nearest_neighbours(input, k=4)
+    find_closest_data(input, k)
+  end
+
+  private
+
+  def find_closest_data(input, k)
+    begin
+      calculated_distances = []
+
+      @data.each_with_index do |datum, index| #Ye olde english
+        # distance = input.send(@distance_measure, datum)
+        distance = 0
+        for i in 0..( input.length-1 )
+          distance = distance + (input[i] - datum[i])**2
+        end
+        distance = distance**(0.5)
+        calculated_distances << [index, distance, datum]
+      end
+
+      calculated_distances.sort {|x, y| x[1] <=> y[1]}.first(k)
+    rescue NoMethodError
+      raise "Hey, that's not a measurement. Read the README for available measurements"
+    end
+  end
 end
